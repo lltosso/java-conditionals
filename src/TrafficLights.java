8cdb5677da9d1f25460e312735b034ee020d0cef
lltosso <luis.tosso@gmail.com>
2024-11-27 13:48:57 +0100

    Traffic Lights did

--- src/TrafficLights.java
+++ src/TrafficLights.java
@@ -1,15 +1,32 @@
 public class TrafficLights {
     public static void main(String[] args) {
+        String color = "azul";
+        switch (color.toLowerCase()) {
 
-        //Escribe un programa que basado en el color del semáforo verde, amarillo o rojo determine las acciones de un conductor 
-        
-        //Si es verde imprime en la terminal: Puedes seguir
-        //Si es amarillo imprime en la terminal: Ten cuidado
-        //Si es rojo imprime en la terminal: ¡¡¡Para!!!
-        //Si es otro color imprime en la terminal: El semáforo está dañado
+            case "verde":
+                System.out.println("Puedes seguir");
+                break;
+            case "amarillo":
+                System.out.println("Ten cuidado");
+                break;
+            case "rojo":
+                System.out.println("¡¡¡Para!!!");
+                break;
+            default:
+                System.out.println("El semaforo esta dañado");
 
-        //Cambia el valor de la variable, corre el programa y mira el resultado 
+
+                //Escribe un programa que basado en el color del semáforo verde, amarillo o rojo determine las acciones de un conductor
+
+                //Si es verde imprime en la terminal: Puedes seguir
+                //Si es amarillo imprime en la terminal: Ten cuidado
+                //Si es rojo imprime en la terminal: ¡¡¡Para!!!
+                //Si es otro color imprime en la terminal: El semáforo está dañado
+
+                //Cambia el valor de la variable, corre el programa y mira el resultado
+
+        }
 
     }
-    
+
 }
